f8c85d523c7ff7cfc72decd26fceb1c9d829ec02
bols <bols-blue@@users.sourceforge.jp>
2010-11-07 23:18:57 +0900

    commit

--- src/jp/lnc/test/TestSampleActivity.java
+++ src/jp/lnc/test/TestSampleActivity.java
@@ -3,15 +3,19 @@ package jp.lnc.test;
 import jp.lnc.SampleActivity;
 import android.test.ActivityInstrumentationTestCase2;
 
-public class TestSampleActivity extends ActivityInstrumentationTestCase2<SampleActivity> {
+public class TestSampleActivity extends
+		ActivityInstrumentationTestCase2<SampleActivity> {
 
 	public TestSampleActivity() {
 		super("jp.lnc.test", SampleActivity.class);
 		// TODO Auto-generated constructor stub
 	}
-	 
-	public void testSamle(){
-		
+
+	public void testSamle() {
+
 	}
-	
+
+	public void testSamleError() {
+		fail();
+	}
 }
